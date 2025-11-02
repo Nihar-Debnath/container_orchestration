8177ef0d22bb2b946c398a7b827c192a2b0791ff
niharhEAVR <debnathnihar14@gmail.com>
2025-11-02 18:30:10 +0530

    changes

--- index.ts
+++ index.ts
@@ -18,20 +18,33 @@ app.get("/",(req,res)=>{
     res.status(202).json({
         "greet":"Hello user",
         "route-1":"http://ip:3000/cpu",
-        "route-2":"http://ip:3000/health"
+        "route-2":"http://ip:3000/health",
+        "api/v1":"http://ip:3000/api/v1",
+        "api/v2":"http://ip:3000/api/v2",
+        "api/v3/AWS_ID":"http://ip:3000/api/v3/AWS_ID"
     });
 });
 
 
-app.get("/api/route1",(req,res)=>{
+app.get("/api/v1",(req,res)=>{
     res.status(202).json({
-        "routes":"This is Api Route 1"
+        "vs":"This is Api v-1"
     });
 });
 
-app.get("/api/route2",(req,res)=>{
+app.get("/api/v2",(req,res)=>{
     res.status(202).json({
-        "routes":"This is Api Route 2"
+        "vs":"This is Api v-2"
+    });
+});
+
+app.get("/api/v3/:AWS_ID",(req,res)=>{
+
+    const AWS_ID = req.params.AWS_ID;
+
+    res.status(202).json({
+        "vs":"This is Api v-3",
+        "AWS_ID":AWS_ID
     });
 });
 
